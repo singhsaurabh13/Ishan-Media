b1ca954355f8e51c0191f919707e566bca554b47
singhsaurabh13 <ersaurabh5502@gmail.com>
2025-01-03 19:17:58 +0530

    added fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,23 +10,3 @@ export default {
   plugins: [],
 }
 
-
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust paths as per your project
-  theme: {
-    extend: {
-      keyframes: {
-        slideUp: {
-          '0%': { transform: 'translateY(20px)', opacity: 0 },
-          '100%': { transform: 'translateY(0)', opacity: 1 },
-        },
-      },
-      animation: {
-        'slide-up': 'slideUp 1s ease-out',
-      },
-    },
-  },
-  plugins: [],
-};
-
-
